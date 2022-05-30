f32ea9adbc099eaa1a950cf1c6193611300f3fc0
Adi Nugroho <adi.seal@gmail.com>
2022-05-30 17:33:54 +0700

    Comparing two lists

---
+++ Comparing Two Lists_30_05_2022.py
@@ -0,0 +1,14 @@
+# Author        ->  Adi Nugroho
+# Created at    ->  May 30 2022
+
+# Python 3 code to demonstrate 
+# check if two lists are identical
+# using collections.Counter()
+# as an alternative of using "identical operator (==)"
+
+import collections
+
+def check_equals(lst1, lst2):
+	return collections.Counter(lst1) == collections.Counter(lst2)
+	
+# print(check_equals([4, 5, 6], [4, 5])) --> False
