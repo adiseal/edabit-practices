2b540fb1f80929c9636dfe13daf8a288c16f9ee9
Adi Nugroho <adi.seal@gmail.com>
2022-06-12 12:31:21 +0700

    a function that returns the opposite of the given boolean, as a number

---
+++ Flip the Boolean.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/12/2022
+# a function that returns the opposite of the given boolean, as a number. 
+
+def flip_bool(b):
+    if b == 0 or b == False:
+        return 1 
+    return 0
