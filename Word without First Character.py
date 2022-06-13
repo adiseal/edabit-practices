b08a123c891465333587527ad75fb43030d7c9a6
Adi Nugroho <adi.seal@gmail.com>
2022-06-13 22:44:51 +0700

    a function that takes a word and returns the new word without including the first character

---
+++ Word without First Character.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/13/2022
+# a function that takes a word and returns the new word without including the first character 
+
+def new_word(word):
+    return word[1:]
+    
+# print(new_word("apple")) ➞ "pple"
