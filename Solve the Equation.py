63bf6606f34a6b9ef7e306edeeab5a3e66e0ff57
Adi Nugroho <adi.seal@gmail.com>
2022-06-13 22:42:33 +0700

    a function that takes an equation (e.g. '1+1'), and returns the answer

---
+++ Solve the Equation.py
@@ -0,0 +1,10 @@
+# Author    : Adi Nugroho
+# Date      : Jun/13/2022
+# a function that takes an equation (e.g. "1+1"), and returns the answer
+
+def equation(s):
+	return eval(s)
+
+# print(equation("1+1")) ➞ 2
+# print(equation("7*4-2")) ➞ 26
+# print(equation("1+1+1+1+1")) ➞ 5
