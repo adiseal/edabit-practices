319cca38e453fc7c828ebc7c784a9d7cc1285270
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 14:11:23 +0700

    a function that takes an integer and returns a string with the given number of 'a's in Edabit

---
+++ Edaaaaabit.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that takes an integer and returns a string with the given number of "a"s in Edabit
+
+def how_many_times(num):
+	return "Ed" + "a" * num + "bit"
+	
+# print(how_many_times(5)) ➞ "Edaaaaabit" 
