376cb7ced307ab72fc2e0f3c8f12d3e5eea9e19b
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 15:05:43 +0700

    Return Sole Element in a Set

---
+++ Return Sole Element in a Set.py
@@ -0,0 +1,9 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# Given a set containing one element, return the element
+
+def element_from_set(s):
+	return s.pop()
+
+# print(element_from_set({True})) ➞ True
+# print(element_from_set({11037})) ➞ 11037
