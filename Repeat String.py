80547bf5118ca883b7bdf8c9e033e2630312ffff
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 20:35:33 +0700

    a function that takes a string txt and a number n and returns the repeated string n number of times

--- Repeat String.py
+++ Repeat String.py
@@ -3,6 +3,10 @@
 # a function that takes a string txt and a number n and returns the repeated string n number of times
 
 def repeat_string(txt, n):
-    return txt * n
+    if type(txt) != str:
+        return "Not A String !!"
+    else:
+        return txt * n    
 
-print(repeat_string("Mubashir", 2)) # ➞ "MubashirMubashir"
+# print(repeat_string(1990, 7)) ➞ "Not A String !!"
+# print(repeat_string("Mubashir", 2))  ➞ "MubashirMubashir"
