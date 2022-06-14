4697b3490dbbad043ded41d48b057868fceb4587
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 20:36:23 +0700

    a function that returns 0 if the input is 1, and returns 1 if the input is 0

---
+++ No Conditionals.py
@@ -0,0 +1,11 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+#  a function that returns 0 if the input is 1, and returns 1 if the input is 0
+
+def flip(y):
+	if y == 1:
+		return 0
+	return 1
+	
+# print(flip(1)) ➞ 0
+# print(flip(0)) ➞ 1
