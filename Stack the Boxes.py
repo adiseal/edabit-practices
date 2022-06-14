1c06e30ca2f8cc254fa7075d0624242947e564ad
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 17:58:26 +0700

    a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible

---
+++ Stack the Boxes.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible
+
+def stack_boxes(n):
+	return n ** 2
+	
+# print(stack_boxes(2)) ➞ 4
