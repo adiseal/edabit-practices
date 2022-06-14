6b34fd6fa2b457296de06ce19487625b4dbbdc6f
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 18:06:48 +0700

    a function that takes a list of values and returns the first and last values in a new list

---
+++ Return the First and Last Elements in a List.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that takes a list of values and returns the first and last values in a new list
+
+def first_last(lst):
+    return [ lst[0], lst[-1] ]
+
+# print(first_last([5, 10, 15, 20, 25])) #➞ [5, 25]
