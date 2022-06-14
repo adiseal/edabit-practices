7fc27364f7e7926a576372fc6eedecdaf43862ad
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 14:27:51 +0700

    a function that returns True if a string contains any spaces

---
+++ Check String for Spaces.py
@@ -0,0 +1,8 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that returns True if a string contains any spaces
+
+def has_spaces(txt):
+    return " " in txt    
+
+# print(has_spaces(" ")) ➞ True
