aec44f00139a4e9c151ce7faf24ba3ec282d444a
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 14:20:10 +0700

    a function that accepts a list of numbers and return both the minimum and maximum numbers, in that order (as a list)

---
+++ Find the Smallest and Biggest Numbers.py
@@ -0,0 +1,9 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that accepts a list of numbers and return both the minimum and maximum numbers, in that order (as a list)
+
+def min_max(nums):
+	return [min(nums), max(nums)]
+	
+
+# print(min_max([1, 2, 3, 4, 5])) ➞ [1, 5]
