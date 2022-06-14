81088bc24c6c40f905fee48acf2a3a66228ece07
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 16:42:12 +0700

    Return a random integer N such that a <= N <= b

---
+++ Random Integer Generator.py
@@ -0,0 +1,10 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# Return a random integer N such that a <= N <= b
+
+import random
+
+def random_int(a, b):
+	return random.randint(a, b)
+
+# print(random_int(5, 9)) ➞ 5
