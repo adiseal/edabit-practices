2bfb4d70a00bac63e7c2055a61f66fd719a13d5c
Adi Nugroho <adi.seal@gmail.com>
2022-06-14 08:16:00 +0700

    a function that evaluates an equation

---
+++ Evaluate an Equation.py
@@ -0,0 +1,9 @@
+# Author    : Adi Nugroho
+# Date      : Jun/14/2022
+# a function that evaluates an equation
+
+def eq(evaluate):
+	return int(eval(evaluate))
+    
+# print(eq("1+2")) ➞ 3
+# print(eq("6/(9-7)")) ➞ 3
