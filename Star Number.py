1fdde439b735b7c1e8d86f69754feeecaa6ec9df
Adi Nugroho <adi.seal@gmail.com>
2022-12-21 10:26:32 +0700

    a function that takes a positive integer n and returns the nth 'star number'

---
+++ Star Number.py
@@ -0,0 +1,15 @@
+def star_number(n):
+	return (6 * n * (n - 1) + 1)
+
+
+assert star_number(2) == 13
+# n = 2
+# 2nd star number = 13
+
+assert star_number(3) == 37
+# n = 3
+# 3rd star number = 37
+
+assert star_number(5) == 121
+# n = 5
+# 5th star number = 121
