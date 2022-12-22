4c29a8585d5f497c1c72b543fb72f01f67e90e3d
Adi Nugroho <adi.seal@gmail.com>
2022-12-22 13:14:39 +0700

    a function that will return a string by combining the given character list, not including the unwanted final character

---
+++ C String to C++ String (in Python).py
@@ -0,0 +1,11 @@
+
+def cpp_txt(lst):
+    lst = lst[:-1]
+    return ''.join(lst)
+
+
+assert cpp_txt(["H", "i", "!", "\0"]) == "Hi!"
+
+assert cpp_txt(["H", "e", "l", "l", "o", "!", "\0"]) == "Hello!"
+
+assert cpp_txt(["J", "A", "V", "a", "\0"]) == "JAVa"
