129fec8118bc177cf905f4851647e30dbd845872
Adi Nugroho <adi.seal@gmail.com>
2022-12-26 14:34:31 +0700

    a function that will return a dictionary that shows how much a person needs to spend on needs, wants, and savings

---
+++ 50-30-20 Strategy.py
@@ -0,0 +1,13 @@
+def fifty_thirty_twenty(ati):
+    needs = 0.5 * ati
+    wants = 0.3 * ati
+    savings = 0.2 * ati
+    return {"Needs": needs, "Wants": wants, "Savings": savings}
+
+
+
+assert fifty_thirty_twenty(10000) == { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
+
+assert fifty_thirty_twenty(50000) == { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
+
+assert fifty_thirty_twenty(13450) == { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
