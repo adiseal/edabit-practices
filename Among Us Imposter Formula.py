5d9fa143428666fc6fb0435eb1b64b484e19e096
Adi Nugroho <adi.seal@gmail.com>
2022-12-26 22:48:58 +0700

    a function that calculates the chance of being an imposter

--- Among Us Imposter Formula.py
+++ Among Us Imposter Formula.py
@@ -1,19 +1,30 @@
 import math 
 
+dot_decimal = [".4", ".5", ".6", ".7", ".8", ".9"] 
+ 
 def imposter_formula(i, p):
     if i <= 3 and p <= 10:
-        being_imposter = math.ceil((100 * (i / p)))
-        rounded_number = str(being_imposter)
-        return rounded_number + "%"
+        being_imposter = str(100 * (i / p))
+        split_it = being_imposter[2:4] 
+        if split_it in dot_decimal:
+            being_imposter = float(being_imposter)
+            return str(math.ceil(being_imposter)) + "%"            
+        else:
+            being_imposter = float(being_imposter)
+            return str(math.floor(being_imposter)) + "%"
+            
     else:
         return "The player limit is 10 and the imposter count can only go up to 3"
         
         
-assert imposter_formula(1, 10) == "10%"
-#print(imposter_formula(1, 10))
+#assert imposter_formula(1, 10) == "10%"
+print(imposter_formula(1, 10))
 
-assert imposter_formula(2, 5) == "40%"
-#print(imposter_formula(2, 5))
+#assert imposter_formula(2, 5) == "40%"
+print(imposter_formula(2, 5))
 
-assert imposter_formula(1, 8) == "13%"
-#print(imposter_formula(1, 8))
+#assert imposter_formula(1, 8) == "13%"
+print(imposter_formula(1, 8))
+
+#assert imposter_formula(3, 7) == "43%"
+print(imposter_formula(3,7))
