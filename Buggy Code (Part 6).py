0bd8a225e80b771f517375286be309096b992fa1
Adi Nugroho <adi.seal@gmail.com>
2022-12-29 10:07:10 +0700

    remove numbers from a given string!

---
+++ Buggy Code (Part 6).py
@@ -0,0 +1,16 @@
+def remove_numbers(string):
+    str1 = ""
+    for i in string:
+        if i.isalpha():
+            str1 = str1 + i
+        else:
+            continue
+    return str1
+    
+
+
+assert remove_numbers("mubashir1") == "mubashir"
+
+assert remove_numbers("12ma23tt") == "matt"
+
+assert remove_numbers("e1d2a3b4i5t6") == "edabit"
