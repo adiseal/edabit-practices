dd26ff83ea6603f49040a8f2e95428dfb2ee1da3
Adi Nugroho <adi.seal@gmail.com>
2022-12-30 16:41:08 +0700

    a function that will work as the modulus operator % without using the modulus operator

---
+++ The Modulus Operator Function.py
@@ -0,0 +1,20 @@
+def mod(a, b):
+    # Find quotient (integer part only)
+    quotient = int(a / b)
+    # quotient ->  
+    
+    # Find product
+    product = quotient * b
+    
+    # Find modulus (remainder)
+    modulus = a - product
+    
+    return modulus
+    
+    
+    
+assert mod(5, 2) == 1
+
+assert mod(218, 5) == 3
+
+assert mod(6, 3) == 0
