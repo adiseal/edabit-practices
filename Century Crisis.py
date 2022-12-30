9d2d5600b2cfc24cd630f179c29019b8d659ad69
Adi Nugroho <adi.seal@gmail.com>
2022-12-30 14:33:00 +0700

    return the number of people there will be when the spaceship is complete

---
+++ Century Crisis.py
@@ -0,0 +1,9 @@
+def future_people(P, nP):
+	return P + ((nP * 12) * 30)
+    
+
+assert future_people(256, 2) == 976
+
+assert future_people(3248, 6) == 5408
+
+assert future_people(5240, 3) == 6320
