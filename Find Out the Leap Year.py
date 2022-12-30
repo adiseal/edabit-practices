1e33ab78db34728a66f5e3191d86b321db95d0d5
Adi Nugroho <adi.seal@gmail.com>
2022-12-30 19:42:04 +0700

    a function that determines if the year is a leap year or not

---
+++ Find Out the Leap Year.py
@@ -0,0 +1,12 @@
+def leap_year(year):
+    return year % 4 == 0 and year % 100 != 0
+
+ 
+
+assert leap_year(1900) == False 
+
+assert leap_year(2024) == True 
+
+assert leap_year(1968) == True 
+
+assert leap_year(2020) == True # -> DONE! 
