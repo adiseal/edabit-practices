a329d3e9c21ef2299d15c24af04e7ada4a5fdd27
Adi Nugroho <adi.seal@gmail.com>
2023-01-02 08:47:22 +0700

    a function that takes a number as an argument and returns the square root of that number cubed

---
+++ Cube the Square Root.py
@@ -0,0 +1,11 @@
+import math
+
+def cube_squareroot(num):
+    return (int(math.sqrt(num))) * num 
+        
+
+assert cube_squareroot(81) == 729
+
+assert cube_squareroot(1646089) == 2111932187
+
+assert cube_squareroot(695556) == 580093704
