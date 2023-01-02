4dc8186bf88d5b0aa089885de9f59c8c32f1d1b8
Adi Nugroho <adi.seal@gmail.com>
2023-01-02 11:40:23 +0700

    get specific index from the certain item/element

---
+++ listO.py
@@ -0,0 +1,8 @@
+myList = ["zero", False, "one", "ten", "three", 3, "five", "six", "one thousand", True, 5678, -30]
+
+def certainIndex(n):
+    index = myList.index(n)
+    return index
+
+
+print(certainIndex(-30))
