46241a67cd39d54d2335e984466d0c0bfa823a35
Adi Nugroho <adi.seal@gmail.com>
2023-01-04 11:55:29 +0700

    a function that takes a number n and returns True if it is an Automorphic number, otherwise False

--- Automorphic Numbers.py
+++ Automorphic Numbers.py
@@ -1,4 +1,16 @@
-def is_automorphic(n):
-    if str(n ** 2).endswith(str(n)):
-        return True
-    return False
+def automorphic(n):
+    automorphic_number = str(n * n)
+    return automorphic_number.endswith(str(n))
+    
+
+
+assert automorphic(1) == True
+
+assert automorphic(3) == False
+# 3^2 = 9
+
+assert automorphic(6) == True
+# 6^2 = 36 (ends with 6)
+
+assert automorphic(95) == False
+# 95^2 = 9025 (does not end with 95)
