e825adec9be04951c6799eb397a346076e719c25
Adi Nugroho <adi.seal@gmail.com>
2023-01-06 17:09:17 +0700

    a function that adds the correct parity bit to a binary string

---
+++ Adding Parity Bits.py
@@ -0,0 +1,12 @@
+def add_parity_bit(b):
+    the_ones = b.count("1") 
+    if the_ones % 2 == 0:
+        return b + "0"
+    return b + "1"
+
+
+assert add_parity_bit("0010110") == "00101101"
+
+assert add_parity_bit("1100000") == "11000000"
+
+assert add_parity_bit("1111111") == "11111111"
