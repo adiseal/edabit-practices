6552f5651d5aa13a46027345dc83ed13dbb2a9f1
Adi Nugroho <adi.seal@gmail.com>
2023-01-11 10:47:09 +0700

    a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string, a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string

---
+++ Count Instances of a Character in a String.py
@@ -0,0 +1,9 @@
+def char_count(txt1, txt2):
+    return txt2.count(txt1)
+    
+
+assert char_count("a", "edabit") == 1
+
+assert char_count("c", "Chamber of secrets") == 1
+
+assert char_count("b", "big fat bubble") == 4
