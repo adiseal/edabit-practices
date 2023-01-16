8ec01c5c27005c52c88edad46da73e1c7ad525a3
Adi Nugroho <adi.seal@gmail.com>
2023-01-16 19:55:25 +0700

    a function that takes a string and returns the letters that occur only once

--- Find the Unique Letters.py
+++ Find the Unique Letters.py
@@ -1,5 +1,15 @@
-word = "monopoly"
-#lst = []
-for x in word:    
-    if x in word.count(x) > 1:
-        print(x)
+def find_letters(word):
+    unique_letters = []
+    for i in word:
+        if word.count(i) == 1:
+            unique_letters.append(i)
+    return unique_letters
+                
+        
+        
+        
+assert find_letters("monopoly") == ["m", "n", "p", "l", "y"]
+
+assert find_letters("balloon") == ["b", "a", "n"]
+
+assert find_letters("analysis") == ["n", "l", "y", "i"]
