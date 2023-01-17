472140bcd28ea11196541c91d80395ba320b0128
Adi Nugroho <adi.seal@gmail.com>
2023-01-17 16:08:00 +0700

    a function that returns the number of syllables in a simple string. The string is made up of short repeated words like Lalalalalalala (which would have 7 syllables)

---
+++ Count the Syllables.py
@@ -0,0 +1,10 @@
+def count_syllables(txt):
+	return int(len(txt) / 2)
+    
+    
+    
+count_syllables("Hehehehehehe") == 6
+
+count_syllables("bobobobobobobobo") == 8
+
+count_syllables("NANANA") == 3
