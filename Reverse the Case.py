26f06ca6db3d739ea622dd99c17e98c5f0f2eb12
Adi Nugroho <adi.seal@gmail.com>
2023-01-19 14:03:25 +0700

    Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa

---
+++ Reverse the Case.py
@@ -0,0 +1,9 @@
+def reverse_case(txt):
+    return txt.swapcase()
+    
+    
+assert reverse_case("Happy Birthday") == "hAPPY bIRTHDAY"
+
+assert reverse_case("MANY THANKS") == "many thanks"
+
+assert reverse_case("sPoNtAnEoUs") == "SpOnTaNeOuS"
