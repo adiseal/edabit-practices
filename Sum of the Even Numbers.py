47e8f7ee750947c6e1aa17451f656113eb43b0b9
Adi Nugroho <adi.seal@gmail.com>
2023-01-30 13:52:53 +0700

    Sum if the even numbers

--- Sum of the Even Numbers.py
+++ Sum of the Even Numbers.py
@@ -1,8 +1,5 @@
-#def add_negative_to_n(n):
-#    return sum(range(-100, n + 1, 2))
+def add_even_to_n(n):
+    return sum(range(2, n + 1, 2))
 
-#print(add_even_to_n(13))
+print(add_even_to_n(13))
 # 2 + 4 + 6 + 8 + 10 + 12 = 42
-
-print((-100) + 1)
-
