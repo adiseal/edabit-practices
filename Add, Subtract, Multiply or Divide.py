101ca1454b0ddf8477ef5a2d4f091b9c573278da
Adi Nugroho <adi.seal@gmail.com>
2023-02-05 20:44:15 +0700

    a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24. If none of the operations can give 24, return None

---
+++ Add, Subtract, Multiply or Divide.py
@@ -0,0 +1,18 @@
+def operation(num1, num2):
+    if num1 + num2 == 24:
+        return "added"
+    elif num1 * num2 == 24:
+        return "multiplied"
+    elif num1 - num2 == 24:
+        return "subtracted"
+    elif num1 / num2 == 24:
+        return "divided"
+    else:
+        return None
+
+
+assert operation(15, 9) == "added"
+
+assert operation(26, 2) == "subtracted"
+
+assert operation(11, 11) == None
