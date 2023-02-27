5ef6233fdec227b84f7ebfd97bb8561489e28340
Adi Nugroho <adi.seal@gmail.com>
2023-02-27 13:59:28 +0700

    Given a string of numbers separated by a comma and space, return the total sum of all the numbers

--- Adding Numbers in a String.py
+++ Adding Numbers in a String.py
@@ -1,10 +1,8 @@
 def add_nums(nums):
-    lst_nums = nums.split(",")
-    sumList_nums = []
-    for x in lst_nums:
-        int_nums = int(x)
-        sumList_nums.append(int_nums)
-    return sum(sumList_nums)
-    
+    a = 0
+    nums = nums.split(",")
+    for x in nums:
+        a = a + int(x)
+    return a
 
 print(add_nums("18, 20, 11, -7, -1, -7, -17, -3, 25, 23, 25, 6, 7, -1, -11, -24, -19, -18, 8, 24, 29, 19, 13, 0, -19, -25, -2, 8")) #➞ 82
