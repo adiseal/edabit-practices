d2a0b193824f41e8f6ec2b5b06337c90fd1350b2
Adi Nugroho <adi.seal@gmail.com>
2023-03-02 12:06:58 +0700

    In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided die. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.

---
+++ Tile Teamwork Tactics.py
@@ -0,0 +1,11 @@
+def possible_bonus(a, b):
+    if b > a:
+        a = a + 6
+        if a == b:
+          return None
+        elif a >= b :
+            return True
+        else:
+            return False
+
+print(possible_bonus(7, 6)) 
