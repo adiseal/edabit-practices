10fb9ce43436c32192c206d7d338b508ede35395
Adi Nugroho <adi.seal@gmail.com>
2023-03-03 15:10:32 +0700

    Something which is not true is false, but something which is not not true is true! Create a function where given n number of not, evaluate whether it's True or False

---
+++ Not Not Not True.py
@@ -0,0 +1,7 @@
+def not_not_not(n, b):
+    if b == True:
+        return True if n % 2 == 0 else False
+    elif b == False:
+        return False if n % 2 == 0 else True
+        
+print(not_not_not(2, False))
