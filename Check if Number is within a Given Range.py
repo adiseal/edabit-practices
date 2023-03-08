9e65498a33a7ad6ec8bc415ed05751d6dd2f65ff
Adi Nugroho <adi.seal@gmail.com>
2023-03-08 10:57:02 +0700

    Check if Number is within a Given Range

--- Check if Number is within a Given Range.py
+++ Check if Number is within a Given Range.py
@@ -1,7 +1,8 @@
 def is_in_range(n, r):
-    if n >= r["min"] and n <= r["max"]:
-        return True
-    else:
-        return False
-        
-print(is_in_range(1.8, { "min": 1.25, "max": 1.75 })) 
+    for v in range(r["min"], r["max"]):
+        if n == v:
+            return True
+        else:
+            return False
+            
+print(is_in_range(4, { "min": 0, "max": 5 })) 
