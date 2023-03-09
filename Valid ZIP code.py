c8582399f7d2f0b7bacbe8ec20c6ff1690f3b500
Adi Nugroho <adi.seal@gmail.com>
2023-03-09 22:34:04 +0700

    def valid(txt):

---
+++ Valid ZIP code.py
@@ -0,0 +1,12 @@
+def valid(txt):
+    if txt.isdigit():
+        if len(txt) == 4 or len(txt) == 6:
+            for i in txt:
+                if i == " ":
+                    return False
+                else:
+                    return True
+        else:
+            return False
+    else:
+        return False
