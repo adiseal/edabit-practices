3e0a08b96af1993ab87253d17d767ed98dfc7db1
Adi Nugroho <adi.seal@gmail.com>
2023-03-10 21:01:40 +0700

    Reverse Psychology

--- Reverse Psychology.py
+++ Reverse Psychology.py
@@ -1,8 +1,8 @@
 def reverse_psychology(s):
-    a = "Do Not"
+    a = "Do not "                                             # do as question says
     if a in s:
         return s[7:len(s)]
     else:
-        return "Do not " + s[:] + "."
+        return "Do not " + s[:]
         
-print(reverse_psychology("eat your lunch")) # "Do not eat your lunch."
+print(reverse_psychology("Do not wash the dishes"))
