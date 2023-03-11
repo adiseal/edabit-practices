9bb77afb448f5b0af1e53cebc50b9fa409341f15
Adi Nugroho <adi.seal@gmail.com>
2023-03-11 21:28:58 +0700

    A number added with its additive inverse equals zero. Create a function that returns a list of additive inverses

--- Additive Inverse.py
+++ Additive Inverse.py
@@ -1,3 +1,10 @@
 def additive_inverse(lst):
-    return [-x for x in lst]
-     
+    index = 0
+    output = []
+    while index < len(lst):
+        s = lst[index] * -1
+        output.append(s)
+        index += 1
+    return output
+
+print(additive_inverse([5, -7, 8, 3])) # [-5, 7, -8, -3]
