d8c38abc7a41b00dd35533be8476deb51de5b0c0
Adi Nugroho <adi.seal@gmail.com>
2023-03-16 18:01:01 +0700

    Given a list of either entirely odd integers or entirely even integers except for a single Outlier Number, create a function to return this number.

---
+++ Outlier Number.py
@@ -0,0 +1,18 @@
+def outlier_number(lst):
+    counteven = 0
+    countodd = 0
+    for i in lst:
+        if i % 2 == 0:
+            counteven = counteven + 1
+        else:
+            countodd = countodd + 1
+    if counteven < countodd:
+        for j in lst:
+            if j % 2 == 0:
+                return j
+    else:
+        for j in lst:
+            if j % 2 != 0:
+                return j
+                
+print(outlier_number([4, 1, 3, 5, 9])) # 4
