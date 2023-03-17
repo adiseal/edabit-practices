d703e5b278daaef71b87d705f81078d599256bf8
Adi Nugroho <adi.seal@gmail.com>
2023-03-17 13:04:20 +0700

    A Narcissistic Number is a number that is the sum of its own digits each raised to the power of the number of digits

---
+++ Narcissistic Number.py
@@ -0,0 +1,8 @@
+def is_narcissistic(n):
+    a = len(str(n))
+    b = 0
+    for i in str(n):
+        b = b + int(i) ** a
+    return True if b == n else False
+	
+print(is_narcissistic(370)) # True
