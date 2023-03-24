e857a0597fd704daf5e48f1355f9bd092b4f192e
Adi Nugroho <adi.seal@gmail.com>
2023-03-24 15:54:07 +0700

    The Reverser takes a string as input and returns that string in reverse order, with the opposite case

---
+++ The Reverser.py
@@ -0,0 +1,11 @@
+def reverse(txt):
+    t = ""
+    for i in txt:
+        if i.isupper():
+            t = i.lower() + t
+        elif i.islower():
+            t = i.upper() + t
+        else: t = i + t
+    return t
+    
+print(reverse("Hello World")) # "DLROw OLLEh"
