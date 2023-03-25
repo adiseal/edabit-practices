66f54925c489c19e7bb55b39e6af688c3bb5ce88
Adi Nugroho <adi.seal@gmail.com>
2023-03-25 16:12:57 +0700

    a function that takes a single string as argument and returns an ordered list containing the indices of all capital letters in the string

---
+++ Return the Index of All Capital Letters.py
@@ -0,0 +1,12 @@
+def index_of_caps(word):
+	count = -1
+	a = []
+	for i in word:
+		count = count + 1
+		if i.isupper():
+			a = a + [count]
+		else:
+			continue
+	return a
+    
+print(index_of_caps("eQuINoX")) # [1, 3, 4, 6]
