4dedb47f24446501458ccbad72ef30c8482be6d8
Adi Nugroho <adi.seal@gmail.com>
2023-03-30 09:18:59 +0700

    a function that returns the number of users in a chatroom based on the following rules

---
+++ Chat Room Status.py
@@ -0,0 +1,12 @@
+def chatroom_status(users):
+    count = 0
+    for i in users:
+        count = count + 1
+    if count == 0:
+        return "no one online"
+    elif count == 1:
+        return "user1 online"
+    elif count == 2:
+        return "user1 and user2 online"
+    else:
+        return users[0]+users[1]+"and n-2 more online"
