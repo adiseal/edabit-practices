5b0e326dcfdb1cf0f44687d7b8afdcc86c0ad84a
Adi Nugroho <adi.seal@gmail.com>
2023-04-06 19:15:38 +0700

    a function that returns the lexicographically first and lexicographically last rearrangements of a lowercase string

---
+++ Lexicographically First and Last.py
@@ -0,0 +1,13 @@
+def first_and_last(s):
+	a = []
+	b = []
+	for i in s:
+		a = a + [i]
+	a.sort()
+	for j in a:
+		b = [j] + b
+	a = "".join(a)
+	b = "".join(b)
+	return [a,b]
+	
+print(first_and_last("scoop")) # ["coops", "spooc"]
