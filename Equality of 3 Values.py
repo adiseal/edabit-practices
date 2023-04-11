968aafc543c54c89e3f05bb8cb353b51bdcff753
Adi Nugroho <adi.seal@gmail.com>
2023-04-11 22:42:26 +0700

    a function that takes three integer arguments (a, b, c) and returns the amount of integers which are of equal value

---
+++ Equality of 3 Values.py
@@ -0,0 +1,11 @@
+def equal(a, b, c):
+    q = [a,b,c]
+    w = []
+    for i in q:
+        w = w + [q.count(i)]
+    if max(w) == 1:
+        return 0
+    else:
+        return max(w)
+        
+print(equal(3, 4, 1)) # 0
