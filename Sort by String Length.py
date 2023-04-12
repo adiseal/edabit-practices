7573a054e9b6abfa9a41a3db575fd07f47384e86
Adi Nugroho <adi.seal@gmail.com>
2023-04-12 16:35:50 +0700

    a function that returns a list of strings sorted by length in ascending order

---
+++ Sort by String Length.py
@@ -0,0 +1,12 @@
+def sort_by_length(lst):
+	d = {}
+	key = []
+	mai = []
+	for i in lst:
+		d[len(i)] = i
+	for k,v in d.items():
+		key = key + [k]
+	key.sort()
+	for j in key:
+		mai = mai + [d[j]]
+	return 
