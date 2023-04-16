fc4ebadd9eb63d13e52acd57d6735bb0ac97f534
Adi Nugroho <adi.seal@gmail.com>
2023-04-16 17:54:10 +0700

    Create a function that returns True if the given string has any of the following:

---
+++ Alphanumeric Restriction.py
@@ -0,0 +1,9 @@
+def alphanumeric_restriction(s):
+	if s.isalpha() == True:
+		return True
+	elif s.isdigit() == True:
+		return True
+	else:
+		return False
+        
+print(alphanumeric_restriction("123454321")) # True
