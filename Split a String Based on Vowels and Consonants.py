dbcf31e71468ad3b360c78de0d694c99c7099a5d
Adi Nugroho <adi.seal@gmail.com>
2023-04-17 10:36:53 +0700

    a function that takes a string, breaks it up and returns it with vowels first, consonants second. For any character that's not a vowel (like special characters or spaces), treat them like consonants

---
+++ Split a String Based on Vowels and Consonants.py
@@ -0,0 +1,12 @@
+def split(txt):
+    b = ""
+    c = ""
+    a = ["a","e","i","o","u"]
+    for i in txt:
+        if i in a:
+            b = b + i
+        else:
+            c = c + i
+    return b + c
+    
+print(split("What's the time?")) # "aeieWht's th tm?"
