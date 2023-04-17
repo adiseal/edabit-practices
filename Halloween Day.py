46d1fec1cb4d1e81133819e87afb1c2f0e0d1127
Adi Nugroho <adi.seal@gmail.com>
2023-04-17 10:41:08 +0700

    Hallloooooweeeennnnn

---
+++ Halloween Day.py
@@ -0,0 +1,8 @@
+def halloween(dt):
+    a = str(dt)
+    if a[4:] == "/10/31":
+        return "Bonfire toffee"
+    else:
+        return "toffee"
+        
+print(halloween("2013/10/31")) # "Bonfire toffee"
