0f6c00071c95e7404987a8e65d8acad9c070b0a4
Adi Nugroho <adi.seal@gmail.com>
2023-04-22 17:43:32 +0700

    Hamming distance is the number of characters that differ between two strings

---
+++ Hamming Distance.py
@@ -0,0 +1,8 @@
+def hamming_distance(txt1, txt2):
+	count = 0
+	for i in range(len(txt1)):
+		if txt1[i] != txt2[i]:
+			count = count+1
+	return count
+    
+print(hamming_distance("abcde", "bcdef")) # 5
