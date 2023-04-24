8ba484ba53267570dbd3e8ca8717b1a94c1abe87
Adi Nugroho <adi.seal@gmail.com>
2023-04-24 17:43:04 +0700

    a function that returns the longest sequence of consecutive zeroes in a binary string

---
+++ Longest Sequence of Consecutive Zeroes.py
@@ -0,0 +1,12 @@
+def longest_zero(s):
+	a = s.split("1")
+	l = []
+	d = {}
+	for j in a:
+		l = l + [len(j)]
+		d[len(j)] = j
+	for k,v in d.items():
+		if k == max(l):
+			return v
+            
+print(longest_zero("01100001011000")) # "0000"
