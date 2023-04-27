9f8aae1098d226bfa4be19ba79241c9db006d9f5
Adi Nugroho <adi.seal@gmail.com>
2023-04-27 19:28:29 +0700

    a function that returns the frequency distribution of a list. This function should return an object, where the keys are the unique elements and the values are the frequency in which those elements occur

---
+++ Frequency Distribution.py
@@ -0,0 +1,8 @@
+def get_frequencies(lst):
+	d = {}
+	l = set(lst)
+	for i in l:
+		d[i]= lst.count(i)
+	return d
+    
+print(get_frequencies([True, False, True, False, False])) # { True: 2, False: 3 }
