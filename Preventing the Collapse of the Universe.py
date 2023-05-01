e8df278e049b723a0cb1ccf5f3970ba00e70f62e
Adi Nugroho <adi.seal@gmail.com>
2023-05-01 12:40:00 +0700

    a function that when given a math expression as a string, return True if at any point, the expression involves dividing by 0

---
+++ Preventing the Collapse of the Universe.py
@@ -0,0 +1,8 @@
+def catch_zero_division(expr):
+    try:
+        c = eval(expr)
+    except ZeroDivisionError:
+        return True
+    return False
+    
+print(catch_zero_division("2 / 0")) # True
