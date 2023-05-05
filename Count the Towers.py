f4480942da79ee6d9ab13ae567a40635e11ab82c
Adi Nugroho <adi.seal@gmail.com>
2023-05-05 18:47:42 +0700

    a function that counts the number of towers

---
+++ Count the Towers.py
@@ -0,0 +1,12 @@
+def count_towers(towers):
+	a = ""
+	for i in towers[-1]:
+		a = a + i
+	return a.count("##")
+    
+print(count_towers([
+  ["                         ##"],
+  ["##             ##   ##   ##"],
+  ["##        ##   ##   ##   ##"],
+  ["##   ##   ##   ##   ##   ##"]
+])) # 6
