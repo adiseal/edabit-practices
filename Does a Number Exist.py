84081350a2aa856b1ee1665a99f95e824fb8d40c
Adi Nugroho <adi.seal@gmail.com>
2023-05-08 17:21:29 +0700

    a function which validates whether a given number exists, and could represent a real life quantity. Inputs will be given as a string

---
+++ Does a Number Exist.py
@@ -0,0 +1,8 @@
+def valid_str_number(n):
+	if n.isdigit() == True:
+		return True
+	elif n.replace(".","",1).isdigit() == True:
+		return True
+	else:	return False
+    
+print(valid_str_number("3.2")) # True
