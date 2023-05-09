cf86ee7e832211310920fe7d66847595497560d3
Adi Nugroho <adi.seal@gmail.com>
2023-05-09 17:27:03 +0700

    a function that takes a string and returns the number of alphanumeric characters that occur more than once

---
+++ Count the Number of Duplicate Characters.py
@@ -0,0 +1,12 @@
+def duplicate_count(txt):
+    a = []
+    count = 0
+    for i in txt:
+        a = a + [txt.count(i)]
+        txt = txt.replace(i,"")
+    for i in a:
+        if i >1:
+            count = count +1
+    return count
+    
+print(duplicate_count("Indivisibilities")) # 2
