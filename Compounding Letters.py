74ebc86ac70c3e16480772164e130293a26e209d
Adi Nugroho <adi.seal@gmail.com>
2023-05-12 19:36:14 +0700

    a function that takes a string and returns a new string with each new character accumulating by +1. Separate each set with a dash

---
+++ Compounding Letters.py
@@ -0,0 +1,10 @@
+def accum(txt):
+	a = ""
+	count = 1
+	for i in txt.upper():
+		a = a + i+ i.lower()*(count-1)
+		a = a + "-"
+		count = count+1
+	return a[:-1]
+	
+print(accum("abcd")) # "A-Bb-Ccc-Dddd"
