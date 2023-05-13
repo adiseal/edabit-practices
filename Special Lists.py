c42d0423c14d5b5b0f1b2ce004b88e842b131d09
Adi Nugroho <adi.seal@gmail.com>
2023-05-13 13:44:51 +0700

    A list is special if every even index contains an even number and every odd index contains an odd number

---
+++ Special Lists.py
@@ -0,0 +1,17 @@
+def is_special_array(lst):
+    count = -1
+    for i in lst:
+        count = count + 1
+        if count % 2 == 0:
+            if i % 2 == 0:
+                continue
+            else:
+                return False
+        else:
+            if i % 2 != 0:
+                continue
+            else:
+                return False
+    return True
+
+print(is_special_array([2, 7, 4, 9, 6, 1, 6, 3])) # True
