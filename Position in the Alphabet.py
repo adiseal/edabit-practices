33d75205872f1cd6906399b90fc4ec650a552313
Adi Nugroho <adi.seal@gmail.com>
2023-05-13 13:48:56 +0700

    Given a number between 1-26, return what letter is at that position in the alphabet

---
+++ Position in the Alphabet.py
@@ -0,0 +1,7 @@
+def letter_at_position(n):
+    alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for i in alpha:
+        if n == int(n):
+            return alpha[int(n)-1].lower()
+        else:
+            return "invalid"
