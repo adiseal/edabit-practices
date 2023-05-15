475f2ac927686d05103d76739c6448b5ea1acb94
Adi Nugroho <adi.seal@gmail.com>
2023-05-15 21:04:21 +0700

    Alliteration refers to a sequence of words that begin with the same letter

---
+++ Amazing Alliteration.py
@@ -0,0 +1,10 @@
+def alliteration_correct(sentence):
+    a = []
+    s = sentence.split()
+    for i in s:
+        print(i)
+        if len(i) >= 4:
+            a = a + [i[0]]
+    return True if len(set(a)) == 1 else False
+	
+print(alliteration_correct("There are many animals.")) # False
