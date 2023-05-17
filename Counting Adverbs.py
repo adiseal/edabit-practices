74f374ebe9bec14cb5a717fb5de5ea828797cd56
Adi Nugroho <adi.seal@gmail.com>
2023-05-17 21:41:09 +0700

    a function that counts the number of adverbs in a sentence. An adverb is a word that ends with ly

---
+++ Counting Adverbs.py
@@ -0,0 +1,9 @@
+def count_adverbs(sentence):
+    s = sentence.split(" ")
+    count = 0
+    for i in s:
+        if i[-2:] == "ly" or i[-3:] =="ly.":
+            count = count + 1
+    return count
+    
+print(count_adverbs("She ran hurriedly towards the stadium.")) # 1
