65de1db66d720cd05b5c6a7692f4f54e09091197
Adi Nugroho <adi.seal@gmail.com>
2023-05-27 17:11:32 +0700

    Given three lists of integers: lst1, lst2, lst3, return the sum of integers which are common in all three lists

---
+++ Three Lists.py
@@ -0,0 +1,17 @@
+def sum_common(lst1, lst2, lst3):
+	a = []
+	b = []
+	z = 0
+	for i in lst1:
+		for j in lst2:
+			if i == j:
+				a = a+[i]
+	for k in a:
+		for l in lst3:
+			if k == l:
+				b = b+[k]
+	for q in b:
+		z = z+q
+	return z
+    
+print(sum_common([1, 2, 3], [5, 3, 2], [7, 3, 2])) # 5
