763b11109e1af7a6bf83c04ddace8bb4c73caa5e
Adi Nugroho <adi.seal@gmail.com>
2023-05-30 16:04:42 +0700

    a function that takes a string road and returns the car that's in first place

---
+++ Who's in First Place.py
@@ -0,0 +1,8 @@
+def first_place(road):
+    for i in range(len(road)-1,0,-1):           # looping in reverse manner
+        if road[i]!= "=":                       # checking element equal to "="
+            return road[i]                      # when not equal print character
+        else:
+            continue
+            
+print(first_place("====b===O===e===U=A==")) # "A"
