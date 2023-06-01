36ba1eeac36788f25f2f05c0fdc5d2869bf5204e
Adi Nugroho <adi.seal@gmail.com>
2023-06-01 16:49:44 +0700

    a function that returns the indices of all occurrences of an item in the list

---
+++ All Occurrences of an Element in a List.py
@@ -0,0 +1,10 @@
+def get_indices(lst, el):
+	count = -1
+	a = []
+	for i in lst:
+		count += 1
+		if i==el:
+			a +=[count]
+	return a
+
+print(get_indices(["a", "a", "b", "a", "b", "a"], "a")) # [0, 1, 3, 5]
