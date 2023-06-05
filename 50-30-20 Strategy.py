288c60ff6305c08b893b7e0d4b73345046babef8
Adi Nugroho <adi.seal@gmail.com>
2023-06-05 20:10:20 +0700

    The 50-30-20 strategy is a simple way to budget, which involves spending 50% of after-tax income on needs, 30% after tax income on wants, and 20% after-tax income on savings or paying off debt

--- 50-30-20 Strategy.py
+++ 50-30-20 Strategy.py
@@ -11,3 +11,5 @@ assert fifty_thirty_twenty(10000) == { "Needs": 5000, "Wants": 3000, "Savings": 
 assert fifty_thirty_twenty(50000) == { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
 
 assert fifty_thirty_twenty(13450) == { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
+
+print(fifty_thirty_twenty(10000)) # { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
