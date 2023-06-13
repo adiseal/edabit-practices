edbf1750780c723e3a337c97647b2b03a47be641
Adi Nugroho <adi.seal@gmail.com>
2023-06-13 12:58:04 +0700

    a function that returns True if there exists at least one number that is larger than or equal to n

--- Exists a Number Higher.py
+++ Exists a Number Higher.py
@@ -1,8 +1,11 @@
-def exists_higher(lst, n):
-	for i in lst:
-		if i >=n:
-			return True
-		else:
-			return False
-            
-print(exists_higher([4, 3, 3, 3, 2, 2, 2], 4)) # True
+def exists_higher(arr, n):
+    if not arr:  # Check if the array is empty
+        return False
+    
+    for num in arr:
+        if num >= n:
+            return True
+    
+    return False
+    
+print(exists_higher([5, 3, 15, 22, 4], 10)) # True
