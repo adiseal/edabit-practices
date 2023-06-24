32777e96b14d9e3f76735b80c291088512f30797
Adi Nugroho <adi.seal@gmail.com>
2023-06-24 16:01:21 +0700

    You have to create a function that takes two integers n and d and makes a list of squares of all numbers from 0...<= n and returns the count of the digits d in the list

--- Simple Counting.py
+++ Simple Counting.py
@@ -1,5 +1,8 @@
 def count_digits(n, d):
-	a = ""
-	for i in range(1, n + 1):
-		a = a + str(i * i)
-	return a.count(str(d))
+    squares = [i**2 for i in range(n + 1)]
+    count = 0
+    for num in squares:
+        count += str(num).count(str(d))
+    return count
+
+print(count_digits(10, 1)) # 4
