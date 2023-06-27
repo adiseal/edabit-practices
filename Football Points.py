2604a4131cc88c9a8bdc4cec3c273d882b905015
Adi Nugroho <adi.seal@gmail.com>
2023-06-27 14:01:35 +0700

    a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far

---
+++ Football Points.py
@@ -0,0 +1,23 @@
+def football_points(wins, draws, losses): # (3,3,2)
+	index = 1 # 1
+	score = [wins,draws,losses]
+	while index <= len(score): # 3
+		if index == 1:
+			wins = score[0] # 3 
+			sum_wins = 3 * wins # 9
+			#print(sum_wins)
+			index += 1
+		elif index == 2:
+			draws = score[1] #3
+			sum_draws = 1 * draws # 3
+			#print(sum_draws)
+			index += 1
+		else:
+		    losses = score[2] #2
+		    sum_losses = 0 * losses # 0
+		    #print(sum_losses)
+		    index += 1
+	result = sum_wins + sum_draws + sum_losses
+	return result
+    
+print(football_points(3, 4, 2)) # 13
