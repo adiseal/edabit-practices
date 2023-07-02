87a682fc52cab44e2e7c9b062d666e8546617051
Adi Nugroho <adi.seal@gmail.com>
2023-07-02 17:04:52 +0700

    Darts is a target game played by throwing feathered darts at a circular board with numbered spaces

--- Primitive Darts Game.py
+++ Primitive Darts Game.py
@@ -1,13 +1,16 @@
+import math
+
 def darts_scoring(x, y):
-    b= [x,y]
-    for i in b:
-        if i >10:
-            return 0
-        elif 10 >= i > 5:
-            return 1
-        elif 5 >= i > 1:
-            return 5
-        elif i<=1:
-            return 10
-            
-print(darts_scoring(3, 2)) # 5
+    # Calculate the distance from the origin (0, 0) using the Pythagorean theorem
+    distance = math.sqrt(x**2 + y**2)
+    
+    if distance > 10:
+        return 0
+    elif distance > 5:
+        return 1
+    elif distance > 1:
+        return 5
+    else:
+        return 10
+        
+print(darts_scoring(0, 0)) # 10
