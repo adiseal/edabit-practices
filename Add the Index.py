331854ebe43d157ced45ee0b8577697e8711a7d7
Adi Nugroho <adi.seal@gmail.com>
2023-07-04 21:15:11 +0700

    Given a list of numbers, create a function which returns the list but with each element's index in the list added to itself

--- Add the Index.py
+++ Add the Index.py
@@ -8,3 +8,4 @@ def add_indexes(lst):
 	
 print(add_indexes([5, 4, 3, 2, 1])) # [5, 5, 5, 5, 5]
 print(add_indexes([0, 0, 0, 0, 0])) # [0, 1, 2, 3, 4]
+print(add_indexes([5, 4, 3, 2, 1])) # [5, 5, 5, 5, 5]
