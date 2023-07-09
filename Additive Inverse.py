4351fc380c43d885ffb8d0f9385586bca88e8ee4
Adi Nugroho <adi.seal@gmail.com>
2023-07-09 16:44:07 +0700

    a function that returns a list of additive inverses

--- Additive Inverse.py
+++ Additive Inverse.py
@@ -9,3 +9,4 @@ def additive_inverse(lst):
 
 print(additive_inverse([5, -7, 8, 3])) # [-5, 7, -8, -3]
 print(additive_inverse([1, 1, 1, 1, 1])) # [-1, -1, -1, -1, -1]
+print(additive_inverse([-5, -25, 35])) # [5, 25, -35]
