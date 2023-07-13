ba3d28eb2cc8b1a47a28509971cf96e72a9bc371
Adi Nugroho <adi.seal@gmail.com>
2023-07-13 19:52:55 +0700

    There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value

--- Flip the Boolean.py
+++ Flip the Boolean.py
@@ -1,8 +1,5 @@
-# Author    : Adi Nugroho
-# Date      : Jun/12/2022
-# a function that returns the opposite of the given boolean, as a number. 
-
-def flip_bool(b):
-    if b == 0 or b == False:
-        return 1 
-    return 0
+def reverse(value):
+    if type(value) == bool:
+        return not value
+    else:
+        return "boolean expected"
--- Return the Remainder from Two Numbers.py
+++ Return the Remainder from Two Numbers.py
@@ -5,3 +5,4 @@ def remainder(x, y):
 print(remainder(1, 3)) # 1
 print(remainder(3, 4)) # 3
 print(remainder(5, 5)) # 0
+print(remainder(7, 2)) # 1
