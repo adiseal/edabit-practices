89048460a1e04b5cd87b9bc7ef199b5178d6003d
Adi Nugroho <adi.seal@gmail.com>
2023-07-16 09:19:47 +0700

    a function to determine if the sum of all the individual even digits are greater than the sum of all the individual odd digits in a string of numbers

---
+++ Even or Odd Which is Greater.py
@@ -0,0 +1,16 @@
+def even_or_odd(s):
+    even_sum = 0
+    odd_sum = 0
+    for char in s:
+        if char.isdigit():
+            if int(char) % 2 == 0:
+                even_sum += int(char)
+            else:
+                odd_sum += int(char)
+    if even_sum > odd_sum:
+        return "Even is greater than Odd"
+    elif odd_sum > even_sum:
+        return "Odd is greater than Even"
+    else:
+        return "Even and Odd are the same"
+
