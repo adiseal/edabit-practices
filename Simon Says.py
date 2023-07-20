fc7a8c1ff305a4579e70d3397869a5ceb106979c
Adi Nugroho <adi.seal@gmail.com>
2023-07-20 13:44:19 +0700

    a function that takes in two lists and returns True if the second list follows the first list by one element, and False otherwise. In other words, determine if the second list is the first list shifted to the right by 1

--- Simon Says.py
+++ Simon Says.py
@@ -1,10 +1,2 @@
 def simon_says(lst1, lst2):
-	max1=max(lst1)
-	min1=min(lst1)
-	max2=max(lst2)
-	min2=min(lst2)
-
-	if max2>max1 or min1>min2:
-		return True
-	else:
-		return False
+    return lst1[:-1] == lst2[1:]
