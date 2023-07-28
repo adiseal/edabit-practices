839d4d12c9816fcd81324beb37b88d161fb6f161
Adi Nugroho <adi.seal@gmail.com>
2023-07-28 21:15:16 +0700

    a function that takes in a list of names and returns the name of the secret society

--- Secret Society.py
+++ Secret Society.py
@@ -2,3 +2,4 @@ def society_name(friends):
     return ''.join(sorted([name[0] for name in friends]))
 
 print(society_name(["Adam", "Sarah", "Malcolm"])) # "AMS"
+print(society_name(["Harry", "Newt", "Luna", "Cho"])) # "CHLN"
