bed8caee4062953eb8fe70f247ca0aca4b949258
Adi Nugroho <adi.seal@gmail.com>
2023-08-02 19:07:24 +0700

    a function to convert a list of percentages to their decimal equivalents

--- Convert to Decimal Notation.py
+++ Convert to Decimal Notation.py
@@ -2,3 +2,4 @@ def convert_to_decimal(percentages):
     return [float(p.strip('%'))/100 for p in percentages]
 
 print(convert_to_decimal(["1%", "2%", "3%"])) # [0.01, 0.02, 0.03]
+print(convert_to_decimal(["45%", "32%", "97%", "33%"])) # [0.45, 0.32, 0.97, 0.33]
