03456d2711561b2c740866eba4e09f61b9e8b4cc
Adi Nugroho <adi.seal@gmail.com>
2023-08-03 22:57:08 +0700

    a function that takes a string and returns a new string with all vowels removed

--- Remove Every Vowel from a String.py
+++ Remove Every Vowel from a String.py
@@ -5,3 +5,5 @@ def remove_vowels(string):
         if char not in vowels:
             new_string += char
     return new_string
+    
+print(remove_vowels("I have never seen a thin person drinking Diet Coke.")) # " hv nvr sn  thn prsn drnkng Dt Ck."
