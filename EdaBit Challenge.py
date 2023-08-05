5d3b5bf831e44fbad35220fec78f60b952d99a6b
Adi Nugroho <adi.seal@gmail.com>
2023-08-05 08:38:13 +0700

    a function that returns the list of numbers from a given range

--- EdaBit Challenge.py
+++ EdaBit Challenge.py
@@ -12,3 +12,4 @@ def eda_bit(start, end):
     return result
 
 print(eda_bit(0, 10)) # ["EdaBit", 1, 2, "Eda", 4, "Bit", "Eda", 7, 8, "Eda", "Bit" ]
+print(eda_bit(14, 20)) # [14,  "EdaBit", 16, 17,  "Eda", 19, "Bit" ]
