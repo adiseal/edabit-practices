3146578c587261395f5ccf33482461b273970b78
Adi Nugroho <adi.seal@gmail.com>
2023-08-07 08:44:48 +0700

    For instance, total_volume([2, 3, 2], [6, 6, 7], [1, 2, 1]) should return 266 since (2 x 3 x 2) + (6 x 6 x 7) + (1 x 2 x 1) = 12 + 252 + 2 = 266

--- Total Volume of All Boxes.py
+++ Total Volume of All Boxes.py
@@ -3,3 +3,5 @@ def total_volume(*boxes):
     for box in boxes:
         volume += box[0] * box[1] * box[2]
     return volume
+
+print(total_volume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1])) # 63
