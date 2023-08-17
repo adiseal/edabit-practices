07cb3b1a0f11d42d80f5d75ca94a43e8ccdce20b
Adi Nugroho <adi.seal@gmail.com>
2023-08-17 16:18:07 +0700

    a function that returns the nth smallest integer (the smallest integer is the first smallest, the second smallest integer is the second smallest, etc)

--- Nth Smallest Integer.py
+++ Nth Smallest Integer.py
@@ -8,3 +8,4 @@ def nth_smallest(lst, n):
 print(nth_smallest([1, 3, 5, 7], 1)) # 1
 print(nth_smallest([1, 3, 5, 7], 3)) # 5
 print(nth_smallest([1, 3, 5, 7], 5)) # None
+print(nth_smallest([7, 3, 5, 1], 2)) # 3
