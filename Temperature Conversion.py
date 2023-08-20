2197a82ab5c64009f2c00c3abef09fa25796a428
Adi Nugroho <adi.seal@gmail.com>
2023-08-20 22:25:51 +0700

    a program that takes a temperature input in celsius and converts it to Fahrenheit and Kelvin. Return the converted temperature values in a list

---
+++ Temperature Conversion.py
@@ -0,0 +1,7 @@
+def temp_conversion(celsius):
+    fahrenheit = round(celsius * 9/5 + 32, 2)
+    kelvin = round(celsius + 273.15, 2)
+    if kelvin < 0:
+        return "Invalid"
+    else:
+        return [fahrenheit, kelvin]
