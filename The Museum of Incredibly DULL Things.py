9fd589b3f9ac9826a5105780c3edf17fa7dc4967
Adi Nugroho <adi.seal@gmail.com>
2023-08-21 17:17:19 +0700

    a function that takes a list of integers and removes the smallest value

--- The Museum of Incredibly DULL Things.py
+++ The Museum of Incredibly DULL Things.py
@@ -7,3 +7,4 @@ def remove_smallest(lst):
 
 print(remove_smallest([1, 2, 3, 4, 5])) # [2, 3, 4, 5]
 print(remove_smallest([5, 3, 2, 1, 4])) # [5, 3, 2, 4]
+print(remove_smallest([2, 2, 1, 2, 1])) # [2, 2, 2, 1]
