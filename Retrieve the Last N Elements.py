793befccd804bbbc222a0081d6b4cbdbb3ec885d
Adi Nugroho <adi.seal@gmail.com>
2023-08-23 21:05:09 +0700

    a function that retrieves the last n elements from a list

--- Retrieve the Last N Elements.py
+++ Retrieve the Last N Elements.py
@@ -9,3 +9,4 @@ def last(lst, n):
 print(last([1, 2, 3, 4, 5], 1)) # [5]
 print(last([4, 3, 9, 9, 7, 6], 3)) # [9, 7, 6]
 print(last([1, 2, 3, 4, 5], 7)) # "invalid"
+print(last([1, 2, 3, 4, 5], 0)) # []
