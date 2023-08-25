90ab7c2305ac06f53411382371192ed2c60ac012
Adi Nugroho <adi.seal@gmail.com>
2023-08-25 11:16:45 +0700

    Fixed quantifiers indicate numbers of characters or expressions to match

--- RegEx XVIII Fixed Quantifiers.py
+++ RegEx XVIII Fixed Quantifiers.py
@@ -1,3 +1,6 @@
 import re
+
 txt = "Hello!... Wait. How goes?..... GoodBye!.."
-pattern = "\.{3,}"
+pattern = r"\.{3,}"  # Using the {3,} fixed quantifier to match 3 or more dots
+result = re.findall(pattern, txt)
+print(result)  # Output: ['...', '.....']
