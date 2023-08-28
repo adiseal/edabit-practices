f04da0c35ef7d9db5f88fb678071fbd700a5e749
Adi Nugroho <adi.seal@gmail.com>
2023-08-28 19:03:27 +0700

    a function that takes the video length and return it in seconds

---
+++ Video Length in Seconds.py
@@ -0,0 +1,5 @@
+def minutes_to_seconds(video_length: str) -> int:
+    minutes, seconds = map(int, video_length.split(':'))
+    if seconds >= 60:
+        return False
+    return minutes * 60 + seconds
