012055cffc44ad6038dfa0ec15fcfcd50f4d0707
Adi Nugroho <adi.seal@gmail.com>
2023-09-09 11:24:26 +0700

    a function that accepts a string as an argument and returns the first non-repeated character

--- Find the First Non-Repeated Character.py
+++ Find the First Non-Repeated Character.py
@@ -8,3 +8,4 @@ print(first_non_repeated_character("g")) # "g"
 print(first_non_repeated_character("it was then the frothy word met the round night")) # "a"
 print(first_non_repeated_character("the quick brown fox jumps then quickly blows air")) # "f"
 print(first_non_repeated_character("hheelloo")) # False
+print(first_non_repeated_character("")) # False
