f207c47d7c01fa9c7f1d0ec3a3c0bb4493dc757f
Adi Nugroho <adi.seal@gmail.com>
2023-09-10 11:02:47 +0700

    a function that converts a dictionary into a list of keys-values tuples

--- Convert Key, Values in a Dictionary to List.py
+++ Convert Key, Values in a Dictionary to List.py
@@ -6,3 +6,9 @@ print(dict_to_list({
   "B": 2,
   "C": 3
 })) # [("B", 2), ("C", 3), ("D", 1)]
+
+print(dict_to_list({
+  "likes": 2,
+  "dislikes": 3,
+  "followers": 10
+})) # [("dislikes", 3), ("followers", 10), ("likes", 2)]
