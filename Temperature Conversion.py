f1e4197894dbf645bf427ea2f4bbdf89ef1944d6
Adi Nugroho <adi.seal@gmail.com>
2023-09-15 20:21:04 +0700

    a program that takes a temperature input in celsius and converts it to Fahrenheit and Kelvin. Return the converted temperature values in a list

--- Temperature Conversion.py
+++ Temperature Conversion.py
@@ -9,3 +9,4 @@ def temp_conversion(celsius):
 print(temp_conversion(0)) # [32, 273.15]
 print(temp_conversion(100)) # [212, 373.15]
 print(temp_conversion(-10)) # [14, 263.15]
+print(temp_conversion(300.4)) # [572.72, 573.55]
