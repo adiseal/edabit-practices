cb91f71932174b76b995dfd94e9054511d1b80b2
Adi Nugroho <adi.seal@gmail.com>
2023-09-17 17:28:06 +0700

    a function that returns the groups in the school by year (as a string), separated with a comma and a space in the form of '1a, 1b, 1c, 1d, 1e, 2a, 2b (....) 5d, 5e, 6a, 6b, 6c, 6d, 6e'

--- Intro to Nested Loops.py
+++ Intro to Nested Loops.py
@@ -5,3 +5,5 @@ def print_all_groups():
         for group in groups:
             result += str(year) + group + ", "
     return result[:-2]
+    
+print(print_all_groups()) # "1a, 1b, 1c, 1d, 1e, 2a, 2b, 2c, 2d, 2e, 3a, 3b, 3c, 3d, 3e, 4a, 4b, 4c, 4d, 4e, 5a, 5b, 5c, 5d, 5e, 6a, 6b, 6c, 6d, 6e "
