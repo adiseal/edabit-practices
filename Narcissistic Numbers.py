8304f53a1df28b32201002105f6f9f144b182b7c
Adi Nugroho <adi.seal@gmail.com>
2023-09-23 23:33:00 +0700

    a positive integer n, implement a function that returns True if the number is narcissistic, and False if it's not

---
+++ Narcissistic Numbers.py
@@ -0,0 +1,10 @@
+def is_narcissistic(n):
+    # Convert the number to string to easily get the digits
+    str_n = str(n)
+    length = len(str_n)
+    
+    # Calculate the sum of each digit raised to the power of the number of digits
+    total = sum(int(digit) ** length for digit in str_n)
+    
+    # Check if the total is equal to the original number
+    return total == n
