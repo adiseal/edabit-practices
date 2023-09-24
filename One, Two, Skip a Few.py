082c66ac08432f50e1f79d06c5c77af523e56209
Adi Nugroho <adi.seal@gmail.com>
2023-09-24 19:13:36 +0700

    a function which calculates how many numbers are missing from an ordered number line

--- One, Two, Skip a Few.py
+++ One, Two, Skip a Few.py
@@ -8,3 +8,4 @@ def how_many_missing(lst):
 print(how_many_missing([1, 3])) # 1
 print(how_many_missing([7, 10, 11, 12])) # 2
 print(how_many_missing([1, 3, 5, 7, 9, 11])) # 5
+print(how_many_missing([5, 6, 7, 8])) # 0
