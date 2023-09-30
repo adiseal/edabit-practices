340baf224635db4917269c90eec2810b32836368
Adi Nugroho <adi.seal@gmail.com>
2023-09-30 22:07:24 +0700

    a function that validates whether a binary string is valid, using parity bits

---
+++ Parity Bit Validation.py
@@ -0,0 +1,10 @@
+def validate_binary(binary):
+    # Count the number of 1s in the binary string excluding the parity bit
+    count_ones = binary[:-1].count('1')
+    # Check if the count of 1s is even or odd
+    if count_ones % 2 == 0:
+        # If even, the parity bit should be 0
+        return binary[-1] == '0'
+    else:
+        # If odd, the parity bit should be 1
+        return binary[-1] == '1'
