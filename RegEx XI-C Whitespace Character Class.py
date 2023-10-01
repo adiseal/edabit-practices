d8c04188c905120965e13c4512791eb41e4b51a0
Adi Nugroho <adi.seal@gmail.com>
2023-10-01 22:33:46 +0700

    Write the regular expression that will match a string if there are no spaces right before the last ending punctuation ?. Use the character class \S in your expression

---
+++ RegEx XI-C Whitespace Character Class.py
@@ -0,0 +1,5 @@
+import re
+
+txt1 = "Can read a spray chart and a balance sheet. 1 part Executive, 1 part entrepreneur, 2 parts geek and 3 parts baseball coach. Too many parts?"
+txt2 = "Can read a spray chart and a balance sheet. 1 part Executive, 1 part entrepreneur, 2 parts geek and 3 parts baseball coach. Too many parts ?"
+pattern = r"\S\?$"
