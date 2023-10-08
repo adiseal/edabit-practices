2b106c1b692c9ef9849a272b1849d84696c4275b
Adi Nugroho <adi.seal@gmail.com>
2023-10-08 19:07:46 +0700

    a function that returns the first index and the last index of a character

--- First and Last Index.py
+++ First and Last Index.py
@@ -5,3 +5,5 @@ def char_index(word, char):
         first_index = word.index(char)
         last_index = len(word) - 1 - word[::-1].index(char)
         return [first_index, last_index]
+
+print(char_index("hello", "l")) # [2, 3]
