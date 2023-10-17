d61f552660891f67b8510b377d8e56189bed0dd0
Adi Nugroho <adi.seal@gmail.com>
2023-10-17 09:29:57 +0700

    a function which takes in a list of numbers and a number to find

--- Sum of Found Indexes.py
+++ Sum of Found Indexes.py
@@ -1,9 +1,10 @@
-def sum_found_indexes(lst, n):
-    res_list = []
-    for x in range(0, len(lst)):
-        if lst[x] == n:
-            res_list.append(x)
-    res = sum(res_list)
-    return res
+    def sum_found_indexes(lst, n):
+        res_list = []
+        for x in range(0, len(lst)):
+            if lst[x] == n:
+                res_list.append(x)
+        res = sum(res_list)
+        return res
     
-print(sum_found_indexes([100, 100, 100, 100, 100], 100))
+print(sum_found_indexes([0, 3, 3, 0, 0, 3], 3)) # 8
+print(sum_found_indexes([1, 2, 3, 4, 5, 6], 3)) # 2
