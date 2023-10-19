65eb110b05fc3f36237dedcbd03c793fc86d9d93
Adi Nugroho <adi.seal@gmail.com>
2023-10-19 12:12:31 +0700

    Alliteration refers to a sequence of words that begin with the same letter. For this exercise, a sentence is correctly alliterated if all words strictly greater than 3 characters begin with the same letter.

--- Amazing Alliteration.py
+++ Amazing Alliteration.py
@@ -1,10 +1,19 @@
+import string
+
 def alliteration_correct(sentence):
-    a = []
-    s = sentence.split()
-    for i in s:
-        print(i)
-        if len(i) >= 4:
-            a = a + [i[0]]
-    return True if len(set(a)) == 1 else False
-	
-print(alliteration_correct("There are many animals.")) # False
+    # Remove punctuation from the sentence
+    sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+    
+    # Split the sentence into words
+    words = sentence.split()
+    
+    # Filter out words that are 3 characters or less
+    long_words = [word for word in words if len(word) > 3]
+    
+    # If there are no long words, return True (since there's no rule being violated)
+    if not long_words:
+        return True
+    
+    # Check that all long words start with the same letter (case insensitive)
+    first_letter = long_words[0][0].lower()
+    return all(word[0].lower() == first_letter for word in long_words)
