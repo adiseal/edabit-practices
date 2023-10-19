7b28014f81bf0aacc2060b5df65c304fe5647f45
Adi Nugroho <adi.seal@gmail.com>
2023-10-19 11:16:53 +0700

    return how many experience points they'll have

--- Edabit Experience Points.py
+++ Edabit Experience Points.py
@@ -2,3 +2,11 @@ def get_xp(d):
     points = {"Very Easy": 5, "Easy": 10, "Medium": 20, "Hard": 40, "Very Hard": 80}
     xp = sum([d.get(k, 0) * v for k, v in points.items()])
     return "{}XP".format(xp)
+
+print(get_xp({
+  "Very Easy" : 89,
+  "Easy" : 77,
+  "Medium" : 30,
+  "Hard" : 4,
+  "Very Hard" : 1
+})) # "2055XP"
