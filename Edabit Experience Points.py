797804c53add0e0274b684065adcedcccded178e
Adi Nugroho <adi.seal@gmail.com>
2023-10-21 21:15:43 +0700

    Edabit Experience Points

--- Edabit Experience Points.py
+++ Edabit Experience Points.py
@@ -18,3 +18,11 @@ print(get_xp({
   "Hard" : 51,
   "Very Hard" : 34
 })) # "7650XP"
+
+print(get_xp({
+  "Very Easy" : 11,
+  "Easy" : 0,
+  "Medium" : 2,
+  "Hard" : 0,
+  "Very Hard" : 27
+})) # "2255XP"
