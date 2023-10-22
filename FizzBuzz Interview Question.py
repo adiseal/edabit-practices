dd3c9127ac549758129b624c375243414dbbb528
Adi Nugroho <adi.seal@gmail.com>
2023-10-22 13:49:19 +0700

    a function that takes a number as an argument and returns 'Fizz', 'Buzz' or 'FizzBuzz'

--- FizzBuzz Interview Question.py
+++ FizzBuzz Interview Question.py
@@ -1,11 +1,3 @@
 def fizz_buzz(num):
-	if num%3==0 and num%5==0:
-		return "FizzBuzz"
-	if num%3==0:
-		return "Fizz"
-	if num%5==0:
-		return "Buzz"
-	else:
-		return '"' + str(num) + '"'
-        
-print(fizz_buzz(3)) # "Fizz"
+    # Nested ternary operator
+	return "FizzBuzz" if num % 3 == 0 and num % 5 == 0 else "Fizz" if num % 3 == 0 else "Buzz" if num % 5 == 0 else str(num) 
