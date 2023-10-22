d92099707b7c4727f4b2ca3a79abe488fe6e46ca
Adi Nugroho <adi.seal@gmail.com>
2023-10-22 13:09:24 +0700

    a function which calculates the remaining mass and the number of years that it took for the substance to decay

--- Radioactive Decay.py
+++ Radioactive Decay.py
@@ -4,3 +4,4 @@ def halflife_calculator(mass, halflife_years, num_halflives):
     return [round(final_mass, 3), total_years]
 
 print(halflife_calculator(1600, 6, 3)) # [200, 18]
+print(halflife_calculator(13, 500, 1)) # [6.5, 500]
