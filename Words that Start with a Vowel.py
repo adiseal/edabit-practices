3e31451cdacec8726289776e4d1f345837d719a9
Adi Nugroho <adi.seal@gmail.com>
2023-10-26 15:59:54 +0700

    a function that retrieves all words that begin with a vowel

--- Words that Start with a Vowel.py
+++ Words that Start with a Vowel.py
@@ -12,3 +12,4 @@ def retrieve(sentence):
 print(retrieve("A simple life is a happy life for me.")) # ["a", "is", "a"]
 print(retrieve("Exercising is a healthy way to burn off energy.")) # ["exercising", "is", "a", "off", "energy"]
 print(retrieve("The poor ostrich was ostracized.")) # ["ostrich", "ostracized"]
+print(retrieve("")) # []
