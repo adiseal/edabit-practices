364ead1c55e8f24089f0c21e2f0f5569a11e0ff8
Adi Nugroho <adi.seal@gmail.com>
2023-10-28 18:52:09 +0700

    a function that takes two inputs: indexes (a list of integers) and string (a string)

--- Index Filtering.py
+++ Index Filtering.py
@@ -2,3 +2,4 @@ def index_filter(indexes, string):
     return ''.join(string[i].lower() for i in indexes)
 
 print(index_filter([2, 3, 8, 11], "Autumn in New York")) # "tune"
+print(index_filter([0, 1, 5, 7, 4, 2], "Cry me a river")) # "creamy"
