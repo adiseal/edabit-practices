34849fa421b68190023b520dc60a342f6502df25
Adi Nugroho <adi.seal@gmail.com>
2023-10-28 14:27:09 +0700

    a function that takes two number strings and returns their sum as a string

--- Adding Numbers.py
+++ Adding Numbers.py
@@ -1,8 +1,4 @@
 def add(n1, n2):
-	if n1 == "" or n2 == "":
+	if (n1 == None or n1 == "") or (n2 == None or n2 == ""):
 		return "Invalid Operation"
-	return int(n1)+int(n2)
-    
-print(add("10", "80")) # "90"
-print(add("111", "111")) # "222
-print(add("", "20")) # "Invalid Operation"
+	return str(int(n1) + int(n2))
