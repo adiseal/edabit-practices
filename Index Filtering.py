a20bc24eb7d4416b1d4bd6f76654b667bc09bdb5
Adi Nugroho <adi.seal@gmail.com>
2023-10-29 20:46:05 +0700

    return another string with the letters of string at each index in indexes in order

--- Index Filtering.py
+++ Index Filtering.py
@@ -3,3 +3,4 @@ def index_filter(indexes, string):
 
 print(index_filter([2, 3, 8, 11], "Autumn in New York")) # "tune"
 print(index_filter([0, 1, 5, 7, 4, 2], "Cry me a river")) # "creamy"
+print()index_filter([9, -9, 2, 27, 36, 6, 5, 13, -1, 2, 0, 30, 2], "That's life, I've got you under my skin") # "frank sinatra"
