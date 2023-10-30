55163b40af867822889bc21abedf0d2091658591
Adi Nugroho <adi.seal@gmail.com>
2023-10-30 16:35:15 +0700

    a function that takes the month and year (as integers) and returns the number of days in that month

--- Days in a Month.py
+++ Days in a Month.py
@@ -4,4 +4,5 @@ def day_amount(month, year):
     return calendar.monthrange(year, month)[1]
     
 print(day_amount(2, 2018)) # 28
-print(day_amount(4, 654)) # 
+print(day_amount(4, 654)) # 30
+print(day_amount(2, 200)) # 28
