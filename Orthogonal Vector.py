2a7c9484c443e25c2598b2d9bc27e93523da4ee9
Adi Nugroho <adi.seal@gmail.com>
2023-10-31 14:10:54 +0700

    a function that takes two vectors as lists and checks if the two vectors are orthogonal or not

--- Orthogonal Vector.py
+++ Orthogonal Vector.py
@@ -3,3 +3,5 @@ def is_orthogonal(first, second):
     dot_product = sum(i*j for i, j in zip(first, second))
     # Check if the dot product is zero
     return dot_product == 0
+
+print(is_orthogonal([1, 2], [2, -1])) # True
