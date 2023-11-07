8fb5cf14abe84b558a8e08d4739795446b5a4d07
Adi Nugroho <adi.seal@gmail.com>
2023-11-07 12:32:59 +0700

    a function that reverses the sublist between the start and end index (inclusive)

--- Ranged Reversal.py
+++ Ranged Reversal.py
@@ -2,3 +2,4 @@ def ranged_reversal(lst, start, end):
     return lst[:start] + lst[start:end+1][::-1] + lst[end+1:]
 
 print(ranged_reversal([1, 2, 3, 4, 5, 6], 1, 3)) # [1, 4, 3, 2, 5, 6]
+print(ranged_reversal([1, 2, 3, 4, 5, 6], 0, 4)) # [5, 4, 3, 2, 1, 6]
