953c909fe50a0d7405a010b489eced734efb28d4
Adi Nugroho <adi.seal@gmail.com>
2023-11-08 20:53:47 +0700

    Fullname and Email

---
+++ Fullname and Email.py
@@ -0,0 +1,6 @@
+class Employee:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.fullname = firstname + ' ' + lastname
+        self.email = (firstname + '.' + lastname + '@company.com').lower()
