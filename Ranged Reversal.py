d463861bf6d4bc3f5569db773cf4b6dd653e46b7
Adi Nugroho <adi.seal@gmail.com>
2023-11-08 20:44:42 +0700

    Range Reversal

--- Ranged Reversal.py
+++ Ranged Reversal.py
@@ -3,3 +3,4 @@ def ranged_reversal(lst, start, end):
 
 print(ranged_reversal([1, 2, 3, 4, 5, 6], 1, 3)) # [1, 4, 3, 2, 5, 6]
 print(ranged_reversal([1, 2, 3, 4, 5, 6], 0, 4)) # [5, 4, 3, 2, 1, 6]
+print(ranged_reversal([9, 8, 7, 4], 0, 0)) # [9, 8, 7, 4]
