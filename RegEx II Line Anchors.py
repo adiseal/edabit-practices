f9316fe7f972382fdb58c3825c0ca1c96c876e89
Adi Nugroho <adi.seal@gmail.com>
2023-11-15 18:25:33 +0700

    RegEx II: Line Anchors

--- RegEx II Line Anchors.py
+++ RegEx II Line Anchors.py
@@ -3,3 +3,4 @@ import re
 pattern = "^/users/edabit/.*"
 
 print(bool(re.search(pattern, "/users/edabit/python/regex.py"))) # True
+print(bool(re.search(pattern, "/users/edabitt/python/file.txt"))) # False
