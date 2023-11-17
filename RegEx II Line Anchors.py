56cc7c69087786932cb7e030ad457904eaeb350b
Adi Nugroho <adi.seal@gmail.com>
2023-11-17 15:27:13 +0700

    RegEx II Line Anchors

--- RegEx II Line Anchors.py
+++ RegEx II Line Anchors.py
@@ -4,3 +4,4 @@ pattern = "^/users/edabit/.*"
 
 print(bool(re.search(pattern, "/users/edabit/python/regex.py"))) # True
 print(bool(re.search(pattern, "/users/edabitt/python/file.txt"))) # False
+print(bool(re.search(pattern, "/users/pyter/edabit/file.py"))) # False
