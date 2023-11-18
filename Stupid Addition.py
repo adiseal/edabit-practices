67ee2ff66d2490e99157a9c74f6ede85998d181a
Adi Nugroho <adi.seal@gmail.com>
2023-11-18 16:24:04 +0700

    Stupid Addition

--- Stupid Addition.py
+++ Stupid Addition.py
@@ -1,13 +1,8 @@
 def stupid_addition(a, b):
-	if type(a) and type(b) == int:
-		a = str(a)
-		b = str(b)
-		return a+b
-	elif (type(a)==str and type(b)==int) or (type(a)==int and type(b)==str):
-		return None
-	else:
-		a = int(a)
-		b = int(b)
-		return a+b
-        
-print(stupid_addition(1, 2)) # "12"
+    if type(a) == type(b):
+        if isinstance(a, str):
+            return int(a) + int(b)
+        elif isinstance(a, int):
+            return str(a) + str(b)
+    else:
+        return None
