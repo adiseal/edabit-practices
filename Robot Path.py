3bbed88a9be996763b63cf69bd7291b7a1535201
Adi Nugroho <adi.seal@gmail.com>
2023-11-18 16:18:34 +0700

    Robot Path

--- Robot Path.py
+++ Robot Path.py
@@ -1,9 +1,9 @@
 def robot_path(commands):
-    destination_1 = ['e', 'n', 'e', 'e', 'n']
-    destination_2 = ['w', 'n', 'w', 'n', 'w', 'w', 'n']
+    destination_1 = ["e", "n", "e", "e", "n"]
+    destination_2 = ["w", "n", "w", "n", "w", "w", "n"]
 
-    # Check if the commands end with either of the destinations
-    if commands[-len(destination_1):] == destination_1 or commands[-len(destination_2):] == destination_2:
-        return True
-    else:
-        return False
+    # Filter out the commands that are not in the destinations
+    filtered_commands = [command for command in commands if command in destination_1 or command in destination_2]
+
+    # Check if the filtered commands end with either destination
+    return filtered_commands[-len(destination_1):] == destination_1 or filtered_commands[-len(destination_2):] == destination_2
