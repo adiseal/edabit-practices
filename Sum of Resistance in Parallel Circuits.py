3101a5344e00f3a25a795f44959b37345186d079
Adi Nugroho <adi.seal@gmail.com>
2023-11-22 20:33:30 +0700

    Sum of Resistance in Parallel Circuits

--- Sum of Resistance in Parallel Circuits.py
+++ Sum of Resistance in Parallel Circuits.py
@@ -9,3 +9,4 @@ print(parallel_resistance([10, 20, 10])) # 4
 print(parallel_resistance([500, 500, 500])) # 166.6
 print(parallel_resistance([6, 3, 6])) # 1.5
 print(parallel_resistance([60, 30, 20])) # 10
+print(parallel_resistance([16, 4])) # 3.2
