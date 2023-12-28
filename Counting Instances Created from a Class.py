63ccad68c565889986582a0211516f90f4af6097
Adi Nugroho <adi.seal@gmail.com>
2023-12-28 17:52:38 +0700

    Counting Instances Created from a Class

---
+++ Counting Instances Created from a Class.py
@@ -0,0 +1,7 @@
+class Composer:
+    count = 0
+    def __init__(self, name, dob, country):
+        self.name = name
+        self.dob = dob
+        self.country = country
+        Composer.count += 1
