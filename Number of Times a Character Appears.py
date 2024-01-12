adeda43012d99bc8cc5c8749c458959ae0818646
Adi Nugroho <adi.seal@gmail.com>
2024-01-12 11:28:34 +0700

    Number of Times a Character Appears

--- Number of Times a Character Appears.py
+++ Number of Times a Character Appears.py
@@ -1,8 +1,6 @@
 def char_appears(sentence, char):
     sentence = sentence.lower()
     char = char.lower()
-    
     words = sentence.split()
-    
     return [word.count(char) for word in words]
 
