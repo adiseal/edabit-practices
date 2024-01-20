3d077a146125df963b3409dfefe8cb8e14f7a303
Adi Nugroho <adi.seal@gmail.com>
2024-01-20 20:16:11 +0700

    Adding Numbers in a String

--- Adding Numbers in a String.py
+++ Adding Numbers in a String.py
@@ -9,3 +9,4 @@ print(add_nums("2, 5, 1, 8, 4")) # 20
 print(add_nums("1, 2, 3, 4, 5, 6, 7")) # 28
 print(add_nums("10")) # 10
 print(add_nums("-12, -8, 2, 11, -16, 16")) # -7
+print(add_nums("25, -4, -15, -7, 27, 12, 29, -6, 20, 9")) # 90
