67e31fbf47f849dac8567180ecfab98301b6db1f
Adi Nugroho <adi.seal@gmail.com>
2024-02-20 16:04:37 +0700

    Sum of Missing Numbers

--- Sum of Missing Numbers.py
+++ Sum of Missing Numbers.py
@@ -1,3 +1,6 @@
 def sum_missing_numbers(lst):
-    start, end = min(lst), max(lst)
-    return sum(range(start, end + 1)) - sum(lst)
+    min_val = min(lst)
+    max_val = max(lst)
+    full_range = set(range(min_val, max_val + 1))
+    missing_numbers = full_range - set(lst)
+    return sum(missing_numbers)
