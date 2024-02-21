fbc539d90227c064c1c3d18158c3295a3631ce8d
Adi Nugroho <adi.seal@gmail.com>
2024-02-21 10:35:02 +0700

    Adding Parity Bits

--- Adding Parity Bits.py
+++ Adding Parity Bits.py
@@ -4,15 +4,4 @@ def add_parity_bit(b):
         return b + "0"
     return b + "1"
 
-
-assert add_parity_bit("0010110") == "00101101"
-
-assert add_parity_bit("1100000") == "11000000"
-
-assert add_parity_bit("1111111") == "11111111"
-
 print(add_parity_bit("0010110")) # "00101101"
-
-print(add_parity_bit("1100000")) # "11000000"
-
-print(add_parity_bit("1111111")) # "11111111"
