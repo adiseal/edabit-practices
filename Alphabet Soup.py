3e68c161b2a4583c8f6389fbe185b8a9ae93229d
Adi Nugroho <adi.seal@gmail.com>
2024-02-24 18:37:34 +0700

    Alphabet Soup

--- Alphabet Soup.py
+++ Alphabet Soup.py
@@ -2,7 +2,3 @@ def alphabet_soup(text):
     return ''.join(sorted(text))
 
 print(alphabet_soup("hello")) # "ehllo"
-print(alphabet_soup("edabit")) # "abdeit"
-print(alphabet_soup("hacker")) # "acehkr"
-print(alphabet_soup("geek")) # "eegk"
-print(alphabet_soup("javascript")) # "aacijprstv"
