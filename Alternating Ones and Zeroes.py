98c4051c1f77bd4adba8631f095713e8f333d84c
Adi Nugroho <adi.seal@gmail.com>
2024-03-04 16:47:56 +0700

    Alternating Ones and Zeroes

--- Alternating Ones and Zeroes.py
+++ Alternating Ones and Zeroes.py
@@ -8,3 +8,4 @@ print(can_alternate("01001")) # True
 print(can_alternate("010001")) # False
 print(can_alternate("0100110111")) # False
 print(can_alternate("10101010")) # True
+print(can_alternate("010101000")) # False
