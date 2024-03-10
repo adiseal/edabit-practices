e6cef5978baaf2fd7a0853bf577a879f9733ce96
Adi Nugroho <adi.seal@gmail.com>
2024-03-10 15:42:28 +0700

    Area of a Triangle

--- Area of a Triangle.py
+++ Area of a Triangle.py
@@ -3,7 +3,8 @@ def tri_area(base, height):
   return area
   
 print(tri_area(3, 2)) # 3
-print(tri_area(5, 4)) # 10
-print(tri_area(10, 10)) # 50
-print(tri_area(0, 60)) # 0
 print(tri_area(7, 4)) # 14
+print(tri_area(10, 10)) # 50
+print(tri_area(5, 4)) # 10
+print(tri_area(0, 60)) # 0
+print(tri_area(12, 11)) # 66
