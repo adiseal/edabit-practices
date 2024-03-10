fc0f36067d66b717d4a29fd008a72966e0aec4bb
Adi Nugroho <adi.seal@gmail.com>
2024-03-10 15:57:26 +0700

    Basic Variable Assignment

--- Basic Variable Assignment.py
+++ Basic Variable Assignment.py
@@ -6,3 +6,4 @@ def name_string(name):
 print(name_string("Mubashir")) # "MubashirEdabit"
 print(name_string("Matt")) # "MattEdabit"
 print(name_string("python")) # "pythonEdabit"
+print(name_string("Mubashir")) # "MubashirEdabit"
