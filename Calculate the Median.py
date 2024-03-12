ee47c30769b3fdbd6dfb1f9b3515dc9af6be4307
Adi Nugroho <adi.seal@gmail.com>
2024-03-12 17:06:21 +0700

    Calculate the Median

--- Calculate the Median.py
+++ Calculate the Median.py
@@ -10,5 +10,3 @@ def median(nums):
     return median
 
 print(median([2, 5, 6, 2, 6, 3, 4])) # 4
-print(median([21.4323, 432.54, 432.3, 542.4567])) # 432.4
-print(median([-23, -43, -29, -53, -67])) # -43
