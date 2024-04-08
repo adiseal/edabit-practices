a77ddc1791ffdd1eb76be2beb64a92c620df6d2d
Adi Nugroho <adi.seal@gmail.com>
2024-04-08 15:33:30 +0700

    Check if a Number is Prime

--- Check if a Number is Prime.py
+++ Check if a Number is Prime.py
@@ -12,7 +12,7 @@ def is_prime(n):
         i += 6
     return True
 
-# Prime numbers => 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+# Prime numbers => 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
 
 print(is_prime(1)) # False
 print(is_prime(2)) # True
@@ -45,3 +45,4 @@ print(is_prime(28)) # False
 print(is_prime(29)) # True
 print(is_prime(30)) # False
 print(is_prime(31)) # True
+print(is_prime(32)) # False
